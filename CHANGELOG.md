de8a27638b274e727925840dfa874261a2ef8f8a
ramesh <ramesh@fcodelabs.com>
2020-03-06 17:40:43 +0530

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## [0.0.1] - March 6, 2020
+
+* Common algorithms and Widgets added
+
---
+++ lib/fcode_common.dart
@@ -0,0 +1,11 @@
+// Copyright 2020 The Fcode Labs Authors. All rights reserved.
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file.
+
+library fcode_common;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
